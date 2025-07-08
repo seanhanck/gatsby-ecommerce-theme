92da4dce184b7fe37def28fbf9937545b2b067d2
seanhanck <35603913+seanhanck@users.noreply.github.com>
2025-07-08 12:06:15 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
+        title={'Essentials for a hot summer!'}
         subtitle={'Discover Autumn Winter 2021'}
         ctaText={'shop now'}
         ctaAction={goToShop}
